d55d518945e03304c08271931d357c966514bd1a
Rithika P <166213945+Rithika2406@users.noreply.github.com>
2024-11-28 18:44:24 +0530

    Add files via upload

---
+++ stringbasic.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Codechef
+{
+	public static void main (String[] args)
+	{
+        Scanner read = new Scanner(System.in);
+        
+        String A = read.next();
+        String B = read.next();
+        String C = read.next();
+        String D = read.next();
+        String E = read.next();
+        System.out.print(A + " " + B + " " + C+ " " + D + " " + E );
+	}
+}
